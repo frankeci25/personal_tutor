ef8ccb822ad7f62c2b76b6e52b0937d7e7ec4add
frankeci25 <francis.addo@icloud.com>
2025-03-24 10:54:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://amakhali.github.io',
   credentials: true
 }));
 
